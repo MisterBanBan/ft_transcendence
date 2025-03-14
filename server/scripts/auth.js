73e9c4a12a194b8d9d1171bc424e84ef43fc03cd
zigeuh <zigeuh13@gmail.com>
2025-03-14 11:34:13 +0100

    Renamed register to signup | Continued sign-up system - Added a 'Confirm password' input - Added email verification (with a Regex) - Added a password verification: 	- At least 8 characters 	- At least 1 upper case 	- At least 1 number 	- At least 1 special character - Registering email + password in a .json file (temporary + need password hashing) - Modified package.json dev rule to ignore users.json so it doesn't restart the server everytime we signup

--- server/scripts/auth.js
+++ server/scripts/auth.js
@@ -1,17 +1,81 @@
 import { fastify } from '../server.js';
 import fastifyFormbody from '@fastify/formbody';
+import fs from 'fs'
 
 fastify.register(fastifyFormbody)
 
-fastify.get('/auth', async function (request, reply) {  
-    return reply.sendFile('auth.html');
+fastify.get('/signup', async function (request, reply) {  
+    return reply.sendFile('signup.html');
 });
 
-fastify.post('/auth', async function (request, reply) {
+fastify.post('/signup', async function (request, reply) {
 
-    const { email, password } = request.body;
-    console.log('Email:', email);
-    console.log('Password:', password);
+    const { email, password, cpassword } = request.body;
 
-    return reply.sendFile('auth.html');
+    const errEmail = validateEmail(email);
+    if (errEmail)
+    {
+        console.log("Invalid email");
+        return reply.redirect('signup');
+    }
+
+    const errPassword = validatePassword(password, cpassword)
+    if (errPassword)
+    {
+        errPassword.forEach(element => {
+            console.log(element);
+        });
+        return reply.redirect('signup');
+    }
+        
+    const userData = { email, password }
+
+    fs.readFile("users.json", "utf-8", (err, data) => {
+        let users = [];
+        if (!err && data) {
+            users = JSON.parse(data);
+        }
+
+        if (users.some(user => user.email === email)) {
+            console.log("Email already in use");
+            return reply.redirect('signup');
+        }
+
+        users.push(userData);
+
+        fs.writeFile("users.json", JSON.stringify(users, null, 2), (err) => {
+            if (err)
+                console.log("An error occurred while registering an user: ", err);
+            else
+                console.log("Correctly registered: ", email);
+        });
+    });
+
+    return reply.redirect('signup');
 });
+
+function validateEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return !emailRegex.test(email);
+}
+
+function validatePassword(password, cpassword) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const isLongEnough = password.length >= minLength;
+    const samePassword = password === cpassword
+
+    if (isLongEnough && hasUpperCase && hasNumber && hasSpecialChar && samePassword) {
+        return false;
+    }
+
+    const errors = [];
+    if (!isLongEnough) errors.push("The password needs at least 8 characters.");
+    if (!hasUpperCase) errors.push("The password needs at least 1 upper case.");
+    if (!hasNumber) errors.push("The password needs at least 1 number.");
+    if (!hasSpecialChar) errors.push("The password needs at least 1 special character.");
+
+    return errors;
+}
