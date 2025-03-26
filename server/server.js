db6b3a1b367258c023575fe6eb54659607c69ada
zigeuh <zigeuh13@gmail.com>
2025-03-26 01:02:58 +0100

    Added fclean rule to Makefile | Moved fastify's plugins registers in registers.js

--- server/server.js
+++ server/server.js
@@ -1,23 +1,19 @@
 import Fastify from 'fastify';
-import fastifyStatic from '@fastify/static';
 import path from 'path';
-import {promises as fs} from 'fs';
+import { promises as fs } from 'fs';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const fastify = Fastify({
-  logger: true
+	logger: true
 });
 
-fastify.register(fastifyStatic, {
-  root: path.join(__dirname, 'srcs'),
-  prefix: '/public/',
-});
+import("./registers.js");
 
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' };
+fastify.get('/', async function handler(request, reply) {
+	return { hello: 'world' };
 });
 
 async function loadScripts(directory) {
@@ -28,14 +24,12 @@ async function loadScripts(directory) {
 		for (const file of filteredFiles) {
 			const filePath = path.join(directory, file);
 
- 			const stat = await fs.stat(filePath);
-  			if (stat.isDirectory())
-  			{
-		   		console.log('\x1b[33m', `Opening directory ${filePath}`, '\x1b[0m');
+			const stat = await fs.stat(filePath);
+			if (stat.isDirectory()) {
+				console.log('\x1b[33m', `Opening directory ${filePath}`, '\x1b[0m');
 				await loadScripts(filePath);
 			}
-			else
-			{
+			else {
 				console.log('\x1b[34m', `Importing file ${file}...`, '\x1b[0m');
 				await import(filePath);
 				console.log('\x1b[32m', `File ${file} correctly imported`, '\x1b[0m');
@@ -45,21 +39,21 @@ async function loadScripts(directory) {
 		console.error('Error reading scripts directory:', error.message);
 		console.error(error.stack);
 		throw error;
- 	}
+	}
 }
 
 async function startServer() {
-  try {
+	try {
 
-	const scriptsDir = path.join(__dirname, 'scripts');
-	await loadScripts(scriptsDir);
+		const scriptsDir = path.join(__dirname, 'scripts');
+		await loadScripts(scriptsDir);
 
-	await fastify.listen({ port: 3000, host: '0.0.0.0' });
-	console.log('Server running on port 3000');
-  } catch (err) {
-	fastify.log.error(err);
-	process.exit(1);
-  }
+		await fastify.listen({ port: 3000, host: '0.0.0.0' });
+		console.log('Server running on port 3000');
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
 }
 
 startServer();
