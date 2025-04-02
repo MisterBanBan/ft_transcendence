96168c9fc231dbc29137dcd12d03cd2704c8f3a0
GHpersonne <anthony.jbn01@gmail.com>
2025-04-02 21:55:48 +0200

    try to connect microservices (users and nginx)

--- services/Nginx/scripts/test/html.js
+++
@@ -1,6 +0,0 @@
-import { fastify } from '../../../Users/app/server.js';
-
-// Serve HTML directly from a route
-fastify.get('/html', async function (request, reply) {
-    return reply.sendFile('index.html');
-});
