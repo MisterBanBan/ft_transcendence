4ceb16dc81c77eba43c87079ad37d85007793a0c
GHpersonne <anthony.jbn01@gmail.com>
2025-04-04 14:23:04 +0200

    nginx work but https://localhost:8443/users/ don't work

---
+++ api/users/src/index.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify';
+
+const server = fastify();
+
+server.get('/api/users', async (request, reply) => {
+    reply.send({ users: ["User1", "User2", "User3"] });
+});
+
+const start = async () => {
+    try {
+        await server.listen({ port: 8080, host: '0.0.0.0' });
+        console.log('users service is running on port 8080');
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
+
+start();
