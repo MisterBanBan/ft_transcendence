5f641701cd0f4eedcaa5f3d6942970d183221ac6
zigeuh <zigeuh13@gmail.com>
2025-05-13 16:41:41 +0200

    Restored nginx with good version + upgraded router system

--- api/nginx/src/router.ts
+++ api/nginx/src/router.ts
@@ -1,15 +1,21 @@
-// import { introduction } from './intro.js'
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   router.ts                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afavier <afavier@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 11:10:24 by afavier           #+#    #+#             */
+/*   Updated: 2025/05/06 11:10:25 by afavier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-interface Route {
-    path: string;
-    title: string;
-    /*Gestion du chargement asynchrone du contenu des templates grace a Promise*/
-    template: (() => Promise<string>) | string;
-}
+import { routes, Route } from './routes.js';
+import { handleRouteComponents } from './route_handler.js';
+//import { introduction } from './intro.js'
 
 class Router {
     private routes: Route[];
-    /*utilisation des elements html*/
     private appDiv: HTMLElement;
     
     constructor(routes: Route[]) {
@@ -23,7 +29,8 @@ class Router {
         /*Cette ligne ajoute un écouteur pour l'événement popstate sur l'objet window. L'événement popstate est déclenché lorsque l'utilisateur utilise les boutons Back ou Forward du navigateur. Lorsque cet événement se produit, la méthode updatePage() est appelée pour mettre à jour le contenu affiché en fonction de l'URL courante, assurant ainsi que l'application réagit correctement aux changements de l'historique sans recharger la page. */
         window.addEventListener("popstate", () => this.updatePage());
     }
-        /*Intercepte les clics*/
+
+    /*Intercepte les clics*/
     private bindLinks(): void {
         document.body.addEventListener("click", (event) => {
             /*seul les liens avec data-link <a href="/home" data-link>Accueil</a>  closest permet de remonter a l'element de datalink*/
@@ -38,107 +45,45 @@ class Router {
             }
         });
     }
+
     public navigateTo(url: string): void {
+        if(!url.startsWith("/")) {
+            console.error("URL not good : ", url);
+            return;
+        }
         history.pushState(null, "",url);
         this.updatePage();
     }
-    public async updatePage(): Promise<void> {
 
-        const path = window.location.pathname;
-        const route = this.routes.find(r => r.path === path) || 
-                      this.routes.find(r => r.path === "*");
-    
-        if (route) {
-            document.title = route.title;
-            let content = route.template;
-            if (typeof content === "function") {
-                try {
-                    content = await content();
-                } catch (error) {
-                    content = "<p>Error failed to up this page </p>";
+    public async updatePage(): Promise<void> {
+        try {
+            const path = window.location.pathname;
+            const route = this.routes.find(r => r.path === path) || 
+                          this.routes.find(r => r.path === "*");
+        
+            if (route) {
+                document.title = route.title;
+                let content = route.template;
+                if (typeof content === "function") {
+                    try {
+                        content = await content();
+                    } catch (error) {
+                        content = "<p>Error failed to up this page </p>";
+                    }
                 }
+                this.appDiv.innerHTML = content;
+                handleRouteComponents(path);
+            } else {
+                this.appDiv.innerHTML = "<h1>404 - Page not found</h1>";
+                return
             }
-            this.appDiv.innerHTML = content;
-        } else {
-            this.appDiv.innerHTML = "<h1>404 - Page not found</h1>";
+        } catch (error) {
+            console.error("Error critical : ", error);
+            this.appDiv.innerHTML = "<h1>Erreur interne</h1>";
         }
+        
     }
 }
-
-
-const routes: Route[] = [
-    {
-        path: "/",
-        title: "Accueil",
-        template: async () => {
-            await new Promise(resolve => setTimeout(resolve, 300));
-            return `
-            <div class="fixed inset-0 overflow-hidden">
-              <div id="pageContainer" class="flex w-[300vw] h-screen overflow-hidden">
-                <div class="w-screen h-screen relative">
-                  <div class="absolute inset-0 w-full h-full bg-[url('/img/fond_outside.jpg')] bg-cover bg-center bg-no-repeat"></div>
-                </div>
-                <div class="w-screen h-screen relative">
-                  <video autoplay loop muted class="absolute inset-0 w-full h-full object-contain bg-black">
-                    <source src="/img/quit.mp4" type="video/mp4">
-                    .
-                  </video>
-                </div>
-                <div id="videoDoor" class="w-screen h-screen relative">
-                  <video autoplay loop muted class="absolute bottom-0 inset-0 w-full h-full object-contain bg-black">
-                    <source src="/img/door.mp4" type="video/mp4">
-                  </video>
-                </div>
-              </div>
-              <div id="player" class="absolute bottom-0 left-0 w-64 h-64 bg-[url('/img/kodama_stop.png')] bg-contain bg-no-repeat z-10"></div>
-                <div id="pressE" class="hidden absolute inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50">
-                <video autoplay loop muted class="w-12 h-12">
-                    <source src="/img/pressE.mp4" type="video/mp4">
-                </video>
-                </div>
-                <script type="module" src="/public/intro.js"></script>
-                
-
-            </div>`;
-            
-
-            
-        }        
-        
-    },
-    {
-        path: "/game",
-        title: "Game",
-        template: async () => {
-            await new Promise(resolve => setTimeout(resolve, 300));
-            return `<div class="w-screen h-screen relative">
-                        <video autoplay loop muted class="absolute inset-0 w-full h-full object-contain bg-black transition-transform duration-500">
-            <source id="menu" src="/img/new_game.mp4" type="video/mp4">
-                </video>
-                </div>
-            `;
-        }
-    },
-    {
-        path: "/Tv",
-        title: "Tv",
-        template: async () => {
-            await new Promise(resolve => setTimeout(resolve, 300));
-            return `<div id="zoom" class="w-screen h-screen relative">
-                        <video autoplay loop muted class="absolute inset-0 w-full h-full object-contain bg-black transition-transform duration-500">
-            <source src="/img/Tv.mp4" type="video/mp4">
-                </video>
-                </div>
-            `;
-        }
-    },
-    {
-        path: "*",
-        title: "404 - Page not found",
-        template: `
-            `
-    }
-];
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -146,9 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const router = new Router(routes);
         router.updatePage();
     } catch (error) {
-        console.error(error);
+        console.error("Wrong init :", error);
+        document.body.innerHTML = "<h1>Appli dumped</h1>";
     }
-
-
-
 })
