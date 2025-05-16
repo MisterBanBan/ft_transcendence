c1d3b784b4c7ca4b438c16f2b542c934a005c126
GHpersonne <anthony.jbn01@gmail.com>
2025-05-16 17:59:19 +0200

    work to profile picture

--- api/users/src/config/cors.ts
+++ api/users/src/config/cors.ts
@@ -5,7 +5,7 @@ export default async function corsConfig(server: FastifyInstance) {
     server.register(cors, {
         origin: process.env.FRONTEND_URL || 'https://localhost',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
+        allowedHeaders: ['Origin', 'X-Requested-With', 'Accept', 'Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
         credentials: true
     });
 }
--- api/users/src/routes/avatar.ts
+++ api/users/src/routes/avatar.ts
@@ -4,17 +4,23 @@ import fs from 'fs';
 import { randomUUID } from 'crypto';
 
 export default async function (server: FastifyInstance) {
-    server.post('/api/users/:id/avatar', async (request, reply) => {
+    server.post('/api/users/1/avatar', async (request, reply) => {
+        console.log('Appel reçu sur /api/users/:id/avatar');
+
         const userId = (request.params as { id: string }).id;
+        console.log(`User ID: ${userId}`);
+
         const data = await request.file();
+        console.log('Fichier reçu:', data?.filename);
 
         if (!data || data.file.truncated) {
-            return reply.code(400).send({ error: 'Fichier invalide ou taille dépassée' });
+            return reply.code(400).send({ error: 'invalid file or size to big' });
         }
 
         const extension = data.filename.split('.').pop();
         const newFilename = `${randomUUID()}.${extension}`;
         const uploadPath = `./uploads/${newFilename}`;
+        console.log(newFilename);
 
         await pipeline(
             data.file,
