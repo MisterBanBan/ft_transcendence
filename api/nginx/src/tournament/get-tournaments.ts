286501c34d33308bb9fde0e4472d2b93742e6620
zigeuh <zigeuh13@gmail.com>
2025-06-27 13:48:38 +0200

    Added auto refresh tournament

--- api/nginx/src/tournament/get-tournaments.ts
+++ api/nginx/src/tournament/get-tournaments.ts
@@ -1,9 +1,10 @@
 import {Component} from "../component";
+import {wait} from "../wait.js";
 
 export class GetTournaments implements Component {
 
 	public init(): void {
-		this.getTournaments();
+		this.getTournaments().then(r => {});
 	}
 
 	destroy(): void {
@@ -13,11 +14,6 @@ export class GetTournaments implements Component {
 	private async getTournaments() {
 
 		try {
-			const response = await fetch("/api/tournament/getTournaments", {
-				method: "GET",
-			});
-
-			const data = await response.json();
 
 			const div = document.getElementById("tournaments");
 
@@ -26,56 +22,86 @@ export class GetTournaments implements Component {
 				return;
 			}
 
-			data.forEach((tournamentObj: { [key: string]: { size: number; players: number } }) => {
-				const name = Object.keys(tournamentObj)[0];
-				const details = tournamentObj[name];
 
-				div.innerHTML += `<div class="bg-white shadow-md w-full max-w-sm">
-									<h2 class="text-2xl font-semibold mb-6 text-center">${name}</h2>
-									<h3 class="text-2xl text-gray-700 mb-4 text-center">${details.players}/${details.size}</h3>
-									<input>
-									 <button type="submit" id="join-tournament-${name}"
-									 class="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200">Join</button>
-								</div>`
+			while (1) {
+				const response = await fetch("/api/tournament/getTournaments", {
+					method: "GET",
+				});
 
-				console.log("Creating eventListener for", name);
-				document.getElementById(`join-tournament-${name}`)!.addEventListener("click", async (e) => {
-					e.preventDefault();
+				const data = await response.json();
 
-					console.log("clicked");
+				if (!response.ok) {
+					console.error(data.message);
+					return;
+				}
 
-					const displayName = document.getElementById('display-name') as HTMLInputElement | undefined;
-					if (!displayName) {
-						console.error("Display name input not found");
-						return;
+				const tournamentsNames: string[] = [];
+
+				data.forEach((tournamentObj: { [key: string]: { size: number; players: number } }) => {
+
+					const name = Object.keys(tournamentObj)[0];
+					const details = tournamentObj[name];
+					const tournamentDiv = document.getElementById(`tournament-${name}`);
+
+					tournamentsNames.push(name);
+
+					if (tournamentDiv) {
+						const element = document.getElementById(`tournament-${name}-size`);
+						if (element) {
+							element.innerText = `${details.players}/${details.size}`;
+						}
 					}
+					else {
+						div.innerHTML += `<div id=tournament-${name} class="bg-white shadow-md w-full max-w-sm">
+										<h2 class="text-2xl font-semibold mb-6 text-center">${name}</h2>
+										<h3 id=tournament-${name}-size class="text-2xl text-gray-700 mb-4 text-center">${details.players}/${details.size}</h3>
+										<input>
+										 <button type="submit" id="join-tournament-${name}"
+										 class="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200">Join</button>
+									</div>`
 
-					const response = await fetch("/api/tournament/join", {
-						method: "POST",
-						headers: {"Content-Type": "application/json"},
-						body: JSON.stringify({ name: name, displayName: displayName.value } as { name: string, displayName: string }),
-					})
+						console.log("Creating eventListener for", name);
+						document.getElementById(`join-tournament-${name}`)!.addEventListener("click", async (e) => {
+							e.preventDefault();
 
-					const data = await response.json();
+							console.log("clicked");
 
-					if (!response.ok) {
-						console.error(data.message);
-						return;
+							const displayName = document.getElementById('display-name') as HTMLInputElement | undefined;
+							if (!displayName) {
+								console.error("Display name input not found");
+								return;
+							}
+
+							const response = await fetch("/api/tournament/join", {
+								method: "POST",
+								headers: {"Content-Type": "application/json"},
+								body: JSON.stringify({ name: name, displayName: displayName.value } as { name: string, displayName: string }),
+							})
+
+							const data = await response.json();
+
+							if (!response.ok) {
+								console.error(data.message);
+								return;
+							}
+						})
+
+						console.log(`Tournoi : ${name}`);
+						console.log(`  - Taille : ${details.size}`);
+						console.log(`  - Joueurs : ${details.players}`);
 					}
-				})
+				});
 
-				console.log(`Tournoi : ${name}`);
-				console.log(`  - Taille : ${details.size}`);
-				console.log(`  - Joueurs : ${details.players}`);
-			});
-
-			if (!response.ok) {
-				console.error(data.message);
-				return;
-			}
-
-			if (data.success) {
-				return window.location.href = '/';
+				for (let child of div.children) {
+					const parts = child.id.split("-");
+					if (parts.length >= 2) {
+						const name = parts[1];
+						if (!tournamentsNames.includes(name)) {
+							child.remove();
+						}
+					}
+				}
+				await wait(10000);
 			}
 
 		} catch (error) {
